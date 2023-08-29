35f2ae340ebc246ceebc65b670a708672214c131
Alumnos <Alumnos@PC61>
2023-08-29 14:31:34 -0300

    Agregar archivos de proyecto.

---
+++ PryRodriguezPOO/FrmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PryRodriguezPOO
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtFuerza = new System.Windows.Forms.TextBox();
+            this.txtDestreza = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblFuerza = new System.Windows.Forms.Label();
+            this.lblDestreza = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.grpCreador = new System.Windows.Forms.GroupBox();
+            this.grpCreador.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(227, 209);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(167, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtFuerza
+            // 
+            this.txtFuerza.Location = new System.Drawing.Point(167, 73);
+            this.txtFuerza.Name = "txtFuerza";
+            this.txtFuerza.Size = new System.Drawing.Size(46, 20);
+            this.txtFuerza.TabIndex = 1;
+            // 
+            // txtDestreza
+            // 
+            this.txtDestreza.Location = new System.Drawing.Point(167, 123);
+            this.txtDestreza.Name = "txtDestreza";
+            this.txtDestreza.Size = new System.Drawing.Size(46, 20);
+            this.txtDestreza.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(17, 26);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblFuerza
+            // 
+            this.lblFuerza.AutoSize = true;
+            this.lblFuerza.Location = new System.Drawing.Point(17, 76);
+            this.lblFuerza.Name = "lblFuerza";
+            this.lblFuerza.Size = new System.Drawing.Size(39, 13);
+            this.lblFuerza.TabIndex = 5;
+            this.lblFuerza.Text = "Fuerza";
+            // 
+            // lblDestreza
+            // 
+            this.lblDestreza.AutoSize = true;
+            this.lblDestreza.Location = new System.Drawing.Point(17, 126);
+            this.lblDestreza.Name = "lblDestreza";
+            this.lblDestreza.Size = new System.Drawing.Size(49, 13);
+            this.lblDestreza.TabIndex = 6;
+            this.lblDestreza.Text = "Destreza";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(32, 247);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(129, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "Aqui va info del personaje";
+            // 
+            // grpCreador
+            // 
+            this.grpCreador.Controls.Add(this.lblNombre);
+            this.grpCreador.Controls.Add(this.txtNombre);
+            this.grpCreador.Controls.Add(this.lblDestreza);
+            this.grpCreador.Controls.Add(this.txtFuerza);
+            this.grpCreador.Controls.Add(this.lblFuerza);
+            this.grpCreador.Controls.Add(this.txtDestreza);
+            this.grpCreador.Location = new System.Drawing.Point(35, 35);
+            this.grpCreador.Name = "grpCreador";
+            this.grpCreador.Size = new System.Drawing.Size(279, 168);
+            this.grpCreador.TabIndex = 8;
+            this.grpCreador.TabStop = false;
+            this.grpCreador.Text = "groupBox1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 450);
+            this.Controls.Add(this.grpCreador);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "FrmMain";
+            this.Text = "Creacion de Personajes";
+            this.grpCreador.ResumeLayout(false);
+            this.grpCreador.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtFuerza;
+        private System.Windows.Forms.TextBox txtDestreza;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblFuerza;
+        private System.Windows.Forms.Label lblDestreza;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.GroupBox grpCreador;
+    }
+}
+
---
+++ PryRodriguezPOO/FrmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryRodriguezPOO
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            ClasPersonaje objeto = new ClasPersonaje();
+            objeto.Nombre = txtNombre.Text;
+            objeto.Fuerza = Convert.ToInt32(txtFuerza.Text);
+            objeto.Destreza = int.Parse (txtDestreza.Text);
+
+            lblInfo.Text += objeto.Nombre + "-" + objeto.Fuerza +"-"+
+                objeto.Destreza+ "\n";
+        }
+    }
+}
